f07adaeead3a90fdbf4b2b7e2020110e879c8b29
AnshuAlawa <alawapappu@gmail.com>
2023-06-01 15:20:45 +0530

    title change of our website

--- welcome.php
+++ welcome.php
@@ -16,9 +16,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Welcome -
-        <?php echo $_SESSION['username'] ?>
-    </title>
+    <title>IPL 2023</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
